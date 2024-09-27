f398b982ac12ddd52d3e2cd8cdf81cd1ae33c0d4
Akib306 <123131218+Akib306@users.noreply.github.com>
2024-09-27 11:45:19 -0600

    Added movement: Holding LEFT ARROW or RIGHT ARROW keys rotates the ship(player).

---
+++ player.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var move_speed := 5.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	# writing to roation
+	# workaround to rebuild contacts due to scaling a collision body
+	self.rotation -= 0.0
+
+	if Input.is_action_pressed("ui_left"):
+		self.rotation -= delta*move_speed 
+	if Input.is_action_pressed("ui_right"):
+		self.rotation += delta*move_speed
