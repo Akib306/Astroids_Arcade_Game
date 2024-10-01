ead6c331c0f2a96285763c9ade8b318126fd4549
Akib306 <123131218+Akib306@users.noreply.github.com>
2024-09-30 22:20:34 -0600

    Added feature: When pressing LEFT ARROW or RIGHT ARROW (rotating left/right), plays an animation at least 3 frames long showing the ship using sideways thrusters.

--- player.gd
+++ player.gd
@@ -6,12 +6,29 @@ class_name Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Ensure all thrusters are initially not visible
+	$LeftThruster.visible = false
+	$RightThruster.visible = false
+	$MainThruster.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Handle left movement
+	if Input.is_action_pressed("ui_left"):
+		$RightThruster.visible = true  # Show right thruster animation
+		$LeftThruster.visible = false  # Hide left thruster
+		
+	elif Input.is_action_pressed("ui_right"):
+		$LeftThruster.visible = true  # Show left thruster animation
+		$RightThruster.visible = false  # Hide right thruster
+	elif Input.is_action_pressed("ui_up"):
+		$MainThruster.visible = true
+	else:
+		# When no keys are pressed, hide the thrusters
+		$LeftThruster.visible = false
+		$RightThruster.visible = false
+		$MainThruster.visible = false
 	
 func _physics_process(delta: float) -> void:
 	# writing to roation
